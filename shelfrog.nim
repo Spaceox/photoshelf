a75afd4d56a5ec66083f3f87c192a4bcb0d31965
Spaceox <91277246+Spaceox@users.noreply.github.com>
2022-09-04 21:44:25 +0200

    small fixes

--- shelfrog.nim
+++ shelfrog.nim
@@ -14,19 +14,17 @@ const lookup = crc32Posix.initLookup()
 # Gets original time/date or last modified time/date
 proc getTimeDate(path: string): seq[string] =
     var exif: string = execProcess(fmt"exiftool.exe -T -d %Y-%m-%d -q -q -m -p '$datetimeoriginal' {path}")
-    if exif == "\'-\'\n":
+    if exif == "\'-\'\n" or "Error" in exif:
         echo "Cannot find original date/time in exif metadata, using file modified date/time."
         exif = execProcess(fmt"exiftool.exe -T -d %Y-%m-%d -q -q -m -p '$filemodifydate' {path}")
-        if exif == "\'-\'\n":
+        if exif == "\'-\'\n" or "Error" in exif:
             echo fmt"Couldn't get date/time, moving into \'{unknownDirectory}\'"
             return @["unknown"]
     return exif.replace("'", "").replace("\n", "").split('-')
 
 proc crcCheck(file1: string, file2: string): bool =
-    echo file1, " ", file2
-    let file2CRC = absolutePath(file2).crcFromFile(crc32Posix, lookup)
     let file1CRC = absolutePath(file1).crcFromFile(crc32Posix, lookup)
-
+    let file2CRC = absolutePath(file2).crcFromFile(crc32Posix, lookup)
     if file1CRC == file2CRC: return true else: return false
 
 # Prepares everything for final move
@@ -57,7 +55,11 @@ proc finalDestination(timedate: seq[string], path: string): tuple[
             dest: fmt"{destDir}/{filename}{splitFile.ext}")
 
 if dirExists(workingDirectory) != true:
-    echo fmt"Cannot find working directory, make sure it's \'{workingDirectory}\'. Exiting..."
+    echo fmt"Cannot find working directory, make sure it's '{workingDirectory}'. Exiting..."
+    quit(QuitFailure)
+
+if fileExists("exiftool.exe") != true:
+    echo "Cannot find exiftool.exe. Exiting..."
     quit(QuitFailure)
 
 let files = collect(newSeq):
@@ -75,6 +77,9 @@ bar[0].total = lenfif # number of iterations
 bar.setup()
 
 for image in files:
+    if image == workingDirectory & "Thumbs.db":
+        echo "Skipping Thumbs.db"
+        continue
     let timedate = getTimeDate("\u0022" & absolutePath(image) & "\u0022")
     let final: tuple[src: string, dest: string] = finalDestination(timedate, image)
     moveFile(final.src, final.dest)
