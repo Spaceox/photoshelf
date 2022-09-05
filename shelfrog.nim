b06de988af880ae06b4d47f754ea93aaeeb5d0b5
Spaceox <91277246+Spaceox@users.noreply.github.com>
2022-09-05 14:29:23 +0200

    Slightly cleaned up the code, and fixed a bug

--- shelfrog.nim
+++ shelfrog.nim
@@ -7,23 +7,30 @@ import sugar
 import w8crc
 import times
 
+# Directory names
 const workingDirectory: string = "working/"
 const duplicatesDirectory: string = "dupes/"
+
+# Lookup table for crc32
 const lookup = crc32Posix.initLookup()
+
+# Self-explanatory duplicate file check using crc
+proc crcCheck(file1: string, file2: string): bool =
+    return file1.crcFromFile(crc32Posix, lookup) == file2.crcFromFile(
+            crc32Posix, lookup)
+
+# Add quotes to a string (generally path) so it works better with exiftool
+proc quotify(str: string): string =
+    return "\u0022" & str & "\u0022"
 
 # Gets original time/date or last modified time/date
 proc getTimeDate(path: string): seq[string] =
-    var exif: string = execProcess(fmt"exiftool.exe -T -d %Y-%m-%d -q -q -m -p '$datetimeoriginal' {path}")
+    var exif: string = execProcess(fmt"exiftool.exe -T -d %Y-%m-%d -q -q -m -p '$datetimeoriginal' {quotify(path)}")
     if exif == "\'-\'\n" or "Error" in exif:
         echo fmt"Original time/date doesn't exist in {path}, using file modified date/time."
-        let fileInfo = getFileInfo(path)
-        exif = format(fileInfo.lastWriteTime, "yyyy-MM-dd")
+        let fileInfo = getLastModificationTime(path)
+        exif = format(fileInfo, "yyyy-MM-dd")
     return exif.replace("'", "").replace("\n", "").split('-')
-
-proc crcCheck(file1: string, file2: string): bool =
-    let file1CRC = absolutePath(file1).crcFromFile(crc32Posix, lookup)
-    let file2CRC = absolutePath(file2).crcFromFile(crc32Posix, lookup)
-    if file1CRC == file2CRC: return true else: return false
 
 # Prepares everything for final move
 proc finalDestination(timedate: seq[string], path: string): tuple[
@@ -38,7 +45,7 @@ proc finalDestination(timedate: seq[string], path: string): tuple[
                 fmt"{splitFile.dir}/{filename}{splitFile.ext}"):
 
             echo fmt"{destDir}/{filename}{splitFile.ext} is the same as {splitFile.dir}/{filename}{splitFile.ext}, moving into {duplicatesDirectory}"
-            createDir(duplicatesDirectory)
+
             return (src: fmt"{splitFile.dir}/{filename}{splitFile.ext}",
             dest: fmt"{duplicatesDirectory}/{filename}{splitFile.ext}")
         else:
@@ -70,15 +77,17 @@ if lenfif == 0:
     echo fmt"{workingDirectory} is empty. Exiting..."
     quit(QuitFailure)
 
+
 var bar: SuruBar = initSuruBar()
 bar[0].total = lenfif # number of iterations
 bar.setup()
 
 for image in files:
-    if image == workingDirectory & "Thumbs.db":
-        echo "Skipping Thumbs.db"
+    if "Thumbs.db" in image or "ZbThumbnail.info" in image or ".json" in image:
+        echo fmt"Skipping {image}"
         continue
-    let timedate = getTimeDate("\u0022" & absolutePath(image) & "\u0022")
+    #let timedate = getTimeDate("\u0022" & absolutePath(image) & "\u0022")
+    let timedate = getTimeDate(image)
     let final: tuple[src: string, dest: string] = finalDestination(timedate, image)
     moveFile(final.src, final.dest)
     inc bar
